cddcd67582063463eabd49617d78ebf2898ce91c
Egor Nesterov <hardsoncore@gmail.com>
2020-03-22 12:12:21 +0200

    Added gulp-concat, updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var { task, parallel, series, src, dest, watch } = require('gulp'),
   uglify = require('gulp-uglify'),
   gulpSass = require('gulp-sass'),
+  concatCss = require('gulp-concat-css'),
   livereload = require('gulp-livereload');
 
 function cleanFunc(cb) {
@@ -8,25 +9,33 @@ function cleanFunc(cb) {
   cb();
 }
 
-function sassFunc() {
-  return src('./assets/sass/**/*.scss')
+function sassToCssFunc() {
+  return src('./assets/styles/sass/**/*.scss')
     .pipe(gulpSass().on('error', gulpSass.logError))
-    .pipe(dest('./assets/css'));
+    .pipe(dest('./assets/styles/css'));
+}
+
+function concatCssFunc() {
+  return src(['./assets/styles/css/*.css', '!./assets/styles/css/bundle.css'])
+    .pipe(concatCss('bundle.css'))
+    .pipe(dest('./assets/styles/css'));
 }
 
 function watchFunc() {
   // подписываемся на событие изменения в файлах
   livereload.listen();
 
-  // watch будет срабатывать сам при каждом изменении
-  watch(['./assets/sass/**/*.scss']).on('change', function() {
+  // watch будет срабатывать сам при каждом изменении во входных файлах scss
+  watch(['./assets/styles/sass/**/*.scss']).on('change', function() {
     console.log(`File 111 was added`);
-    sassFunc();
+    let rebuild = series(sassToCssFunc, concatCssFunc);
+    rebuild();
   });
 
-  watch(['*.html', './assets/css/**/*.css']).on('change', livereload.changed);
+  // watch будет срабатывать сам при каждом изменении в index.html или bundle.css
+  watch(['*.html', './assets/styles/css/bundle.css']).on('change', livereload.changed);
 }
 
 // series запускает все задачи по порядку. пока предыдущая не закончится, новая не стартует
 // parallel позволяет выполнять задачи параллельно
-exports.default = series(cleanFunc, sassFunc, parallel(watchFunc));
+exports.default = series(cleanFunc, sassToCssFunc, concatCssFunc, parallel(watchFunc));
