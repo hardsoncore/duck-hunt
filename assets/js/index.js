caef2cc5d2a1ca401c982a7042118c087850bf5d
Egor Nesterov <hardsoncore@gmail.com>
2020-04-05 12:45:04 +0300

    Added 8bit font, dynamic text changing in index.js. Changed the background image - now it only has grass and trees

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,20 @@
 (function() {
   'use strict';
 
+  const startingAnimationDelay = 5000; // delay for the starting animation
+
+  setTimeout(function () {
+    mainProcessFunc();
+  }, startingAnimationDelay);
+
+  function mainProcessFunc() {
+    addTextOnScreen('Ready?!');
+  }
+
+  function addTextOnScreen(text) {
+    const textTable = document.getElementsByClassName('text-table')[0].textContent = 'ready';
+    // textTable.inner
+    // debugger
+  }
 
 })();
