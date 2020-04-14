1a190d819edc6b4c9e50f71cfdd6dc2409776866
Egor Nesterov <hardsoncore@gmail.com>
2020-04-14 19:14:46 +0300

    optimized duck animation code, added element for showing falling duck

--- assets/js/ducks.js
+++ assets/js/ducks.js
@@ -1,4 +1,15 @@
+// stage parameters
+const stage = document.querySelector("#stage-background");
+const blockWidth = stage.clientWidth;
+const blockHeight = stage.clientHeight;
+
+// ducks
+const flyingDuck = document.querySelector("#duck-flying");
+const fallingDuck = document.querySelector("#duck-falling");
+
+// timing settings
 const delayBetweenDuckFlights = 4000;
+const duckFallingDelay = 1000;
 
 function startDucksFlight(delayBeforeFirstDuckAppears) {
   setTimeout(function() {
@@ -7,45 +18,55 @@ function startDucksFlight(delayBeforeFirstDuckAppears) {
     duckFlight(delayBetweenDuckFlights * 2);
     duckFlight(delayBetweenDuckFlights * 3);
     duckFlight(delayBetweenDuckFlights * 4);
+    afterAllDucks(delayBetweenDuckFlights * 5);
   }, delayBeforeFirstDuckAppears);
 }
 
 function getRandomHorizontalStartPoint() {
-  const stage = document.querySelector("#stage-background");
-  const blockWidth = stage.clientWidth;
-
   return Math.round(Math.random() * blockWidth);
 }
 
 function duckFlight(delay) {
   setTimeout(function() {
-    const duck = document.querySelector("#duck");
     const startPoint = getRandomHorizontalStartPoint();
     const endPoint   = getRandomHorizontalStartPoint();
 
-    duck.style.display = 'block';
+    flyingDuck.style.display = 'block';
     // reset duck rotation before animation starts
-    duck.style.transform = 'scaleX(1)';
-    if ( endPoint < startPoint ) duck.style.transform = 'scaleX(-1)'; // change duck rotation
+    flyingDuck.style.transform = 'scaleX(1)';
+    if ( endPoint < startPoint ) flyingDuck.style.transform = 'scaleX(-1)'; // change duck rotation
 
-    duck.animate([
-      { // from
-        left: startPoint + 'px',
-        bottom: 0
-      },
-      { // to
-        left: endPoint + 'px',
-        bottom: '100vh'
-      }
-    ], {
-      // timing options
-      duration: delayBetweenDuckFlights,
-      // iterations: Infinity
-    });
+    addDuckAnimation(flyingDuck, {x : startPoint, y: blockHeight}, {x : endPoint, y: -100}, delayBetweenDuckFlights);
 
-    duck.addEventListener('click', function() {
-      alert('Перемога!');
+    flyingDuck.addEventListener('click', function(ev) {
+      this.style.display = 'none';
       this.removeEventListener('click', arguments.callee, false);
+
+      fallingDuck.style.display = 'block';
+      addDuckAnimation(fallingDuck, {x: ev.screenX, y: ev.screenY - 100}, {x: ev.screenX, y: blockHeight}, duckFallingDelay);
     });
+  }, delay);
+}
+
+function addDuckAnimation(duck, start, end, duration) {
+  duck.animate([
+    { // from
+      left: start.x + 'px',
+      top: start.y + 'px'
+    },
+    { // to
+      left: end.x + 'px',
+      top: end.y + 'px'
+    }
+  ], {
+    // timing options
+    duration: duration,
+    // iterations: Infinity
+  });
+}
+
+function afterAllDucks(delay) {
+  setTimeout(function() {
+    // showTextOnScreen('You win!', 10000000);
   }, delay);
 }
